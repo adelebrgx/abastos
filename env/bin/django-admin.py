38c310c0244f542d39b01e5d3249a300a62bf27c
adelebrgx <adele.bourgeix@gmail.com>
2020-03-29 13:58:21 -0300

    started django project

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/adelebourgeix/Documents/DCC/confinement/abastos/abastos-/env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
