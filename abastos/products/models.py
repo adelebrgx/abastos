990e61ef6443c75c5250bb4709274b375205c1d0
adelebrgx <adele.bourgeix@gmail.com>
2020-04-01 15:21:45 -0300

    added sell list display

--- abastos/products/models.py
+++ abastos/products/models.py
@@ -5,6 +5,7 @@ from django.db import models
 
 class Product(models.Model):
     name=models.CharField(max_length=20)
+    url=models.CharField(max_length=1000,default="foobar")
 
     def __str__(self):
         return self.name
--- abastos/sell/views.py
+++ abastos/sell/views.py
@@ -3,6 +3,11 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .models import Sell, SellPair
+from products.models import Product
 
 def sell_list_view(request):
-    return render(request,'sell/selllist.html')
+    sellPairs=SellPair.objects.all().order_by('product')
+    print (sellPairs[0].sell.date)
+    sells=Sell.objects.all().order_by('date')
+    return render(request,'sell/selllist.html', {'sells':sells, 'sellpairs': sellPairs})
