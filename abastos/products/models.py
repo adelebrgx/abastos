eb2e218c8cd4c9fc57ddaaf6c4ea6cd60b7a0c90
adelebrgx <adele.bourgeix@gmail.com>
2020-04-02 20:29:32 -0300

    publish sell form works

--- abastos/products/models.py
+++ abastos/products/models.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 from django.db import models
 
 class Product(models.Model):
+    id = models.AutoField(primary_key=True)
     name=models.CharField(max_length=20)
     url=models.CharField(max_length=1000,default="foobar")
 
--- abastos/sell/views.py
+++ abastos/sell/views.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Sell, SellPair
@@ -11,8 +12,38 @@ def sell_list_view(request):
     sellPairs=SellPair.objects.all().order_by('product')
     print (sellPairs[0].sell.date)
     sells=Sell.objects.all().order_by('date')
-    return render(request,'sell/selllist.html', {'sells':sells, 'sellpairs': sellPairs})
+    return render(request,'sell/selllist.html', {'sells':sells, 'sellpairs': sellPairs, 'user':request.user})
 
+@login_required(login_url="/accounts/login/")
 def publish(request):
-    form=forms.CreateSell()
+    sellPairs=SellPair.objects.all().order_by('product')
+    sells=Sell.objects.all().order_by('date')
+    products=Product.objects.all().order_by('name')
+    if request.method=="POST":
+        form=forms.CreateSell(request.POST, request.FILES)
+        if form.is_valid():
+            user=request.user
+            quantity=request.POST['quantity']
+            product=request.POST['product']
+            #print(product)
+            sell=Sell.objects.create(author=user)
+            print(sell)
+            #sell.save()
+            #effective_product=Product.objects.create(name="unknown",url="unknown")
+            for p in products:
+                print(p.id)
+                print(product)
+                if int(p.id)==int(product):
+                    print("hello")
+                    sellPair = SellPair.objects.create(sell=sell,quantity=quantity, product=p)
+                    sellPair.save()
+                    print(sellPair)
+                    print(sellPair.sell)
+
+
+            #
+            #sellPair.save()
+            return render(request, 'sell/selllist.html', {'sells':sells, 'sellpairs': sellPairs,'user':request.user})
+    else:
+        form=forms.CreateSell()
     return render(request, 'sell/publish.html', {'form':form})
