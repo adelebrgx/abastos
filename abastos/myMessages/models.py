49831d08873f210cbecf4412e5a7dd36b56625e7
adelebrgx <adele.bourgeix@gmail.com>
2020-04-16 21:31:50 -0400

    myMessages model

--- abastos/myMessages/models.py
+++ abastos/myMessages/models.py
@@ -2,5 +2,16 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class myMessage(models.Model):
+    id = models.AutoField(primary_key=True)
+    head=models.CharField(max_length=50)
+    content=models.CharField(max_length=1000,default="This message is empty")
+    author=models.ForeignKey(User, related_name="from+")
+    recipient=models.ForeignKey(User, related_name="to+")
+    date=models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.head+" from "+ str(self.author)+" to "+str(self.recipient)
