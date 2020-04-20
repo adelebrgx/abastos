7e6d85805d67083007cd02e28470ce1c61af3721
adelebrgx <adele.bourgeix@gmail.com>
2020-04-19 22:16:00 -0400

    can send message

--- abastos/myMessages/urls.py
+++ abastos/myMessages/urls.py
@@ -5,5 +5,6 @@ app_name='myMessages'
 
 urlpatterns=[
 url(r'^messages-list/$', views.messages_list_view,name="messages_list"),
+url(r'^new-message/$', views.new_message_view,name="new_message"),
 
 ]
--- abastos/myMessages/views.py
+++ abastos/myMessages/views.py
@@ -2,6 +2,32 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from .models import myMessage
+from django.contrib.auth.models import User
+
 
 def messages_list_view(request):
-    return render(request,"myMessages/myMessageslist.html")
+    messages=myMessage.objects.all().order_by('date')
+    user=request.user
+    return render(request,"myMessages/myMessageslist.html", {'messages':messages,'user':user})
+
+
+def new_message_view(request):
+    myuser=request.user
+    users=User.objects.all()
+    messages=myMessage.objects.all().order_by('date')
+    if request.method=="POST":
+        myHead=request.POST['head']
+        myContent=request.POST['content']
+        myAuthor=request.user
+        myRecipient=User.objects.get(username=request.POST['recipient'])
+        print(myHead)
+        print(myContent)
+        print(myAuthor)
+        print(myRecipient)
+        message=myMessage.objects.create(head=myHead, content=myContent, author=myAuthor, recipient=myRecipient)
+        message.save()
+        return render(request, "myMessages/myMessageslist.html", {'messages':messages, 'user':myuser})
+
+
+    return render(request, "myMessages/new_message.html", {'messages':messages, 'myuser':myuser, 'users':users})
