e1a040489f82521318c9765d80e18f69995fb87d
adelebrgx <adele.bourgeix@gmail.com>
2020-05-01 18:06:59 -0400

    changed messages list to a much nicer implementation

--- abastos/myMessages/views.py
+++ abastos/myMessages/views.py
@@ -9,15 +9,15 @@ from django.http import HttpResponse
 
 def messages_list_received_view(request):
     user=request.user
+    type="received"
     messages=myMessage.objects.filter(recipient=user)
-    return render(request,"myMessages/myMessageslist_received.html", {'messages':messages,'user':user})
+    return render(request,"myMessages/myMessageslist.html", {'messages':messages,'user':user,'type':type})
 
 def messages_list_sent_view(request):
     user=request.user
-    print(user)
+    type="sent"
     messages=myMessage.objects.filter(author=user)
-    print(messages)
-    return render(request,"myMessages/myMessageslist_sent.html", {'user':user,'messages':messages})
+    return render(request,"myMessages/myMessageslist.html", {'user':user,'messages':messages,'type':type})
 
 def message_details_view(request,slug):
     message=myMessage.objects.get(slug=slug)
