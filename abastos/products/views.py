59f641d21073e37ca80d11618150c5cc2170cb38
adelebrgx <adele.bourgeix@gmail.com>
2020-05-08 21:42:59 -0400

    fixed product forms

--- abastos/products/views.py
+++ abastos/products/views.py
@@ -18,28 +18,27 @@ def products_list_view(request):
 def publish(request):
     isName=True
     isUrl=True
+    notAlreadyExists=True
     sellpairs_list=SellPair.objects.all().order_by('product')
     if request.method=="POST":
         products=Product.objects.all().order_by('name')
         name=request.POST.get('name')
         url=request.POST.get('url')
-        print(name)
-        print(url)
-        if(correct_name(name)==True):
-            if(correct_url(url)==True):
-                product=Product.objects.create(name=name, url=url)
-                product.save()
-                return render(request, 'products/productslist.html', {'products':products,'user':request.user, 'sellpairs':sellpairs_list})
-            isUrl=False
-        else:
+        already=Product.objects.filter(name=name).count()
+        if(already!=0):
+            notAlreadyExists=False
+        if(correct_name(name)==False):
             isName=False
         if(correct_url(url)==False):
-            if(correct_name(name)==False):
-                isName=False
             isUrl=False
-        return render(request, 'products/publish.html', {'user':request.user,'isName': isName, 'isUrl':isUrl})
+        if(correct_name(name)==True and correct_url(url)==True and notAlreadyExists==True):
+            product=Product.objects.create(name=name, url=url)
+            product.save()
+            return render(request, 'products/productslist.html', {'products':products,'user':request.user, 'sellpairs':sellpairs_list})
+        else:
+            return render(request, 'products/publish.html', {'user':request.user,'isName': isName, 'isUrl':isUrl, 'notAlreadyExists':notAlreadyExists})
 
-    return render(request, 'products/publish.html', {'user':request.user,'isName': isName, 'isUrl':isUrl})
+    return render(request, 'products/publish.html', {'user':request.user,'isName': isName, 'isUrl':isUrl,'notAlreadyExists':notAlreadyExists})
 
 
 def correct_name(s):
@@ -50,38 +49,38 @@ def correct_name(s):
 
 def correct_url(url):
     result=urlparse(url)
-    if(result.scheme!= "" and result.scheme!=""):
+    if(result.scheme!= "" and result.netloc!=""):
         return True
     return False
 
 def product_details(request,slug):
     isName=True
     isUrl=True
+    notAlreadyExists=True
     products=Product.objects.all().order_by('name')
     sellpairs_list=SellPair.objects.all().order_by('product')
     user=request.user
     if request.method=='POST':
-
         product= Product.objects.get(name=slug)
         new_name=request.POST.get('name')
         new_url=request.POST.get('url')
-        if(correct_name(new_name)==True):
-            if(correct_url(new_url)==True):
-                product.name=new_name
-                product.url=new_url
-                product.save()
-                print(correct_name(new_name))
-                return render(request, 'products/productslist.html', {'products':products,'user':request.user, 'sellpairs':sellpairs_list})
-            isUrl=False
-        else:
+        if (correct_name(new_name)==False):
             isName=False
         if(correct_url(new_url)==False):
-            if(correct_name(new_name)==False):
-                isName=False
             isUrl=False
-        return render(request, 'products/product_details.html', {'product':product,'user':request.user,'isName': isName, 'isUrl':isUrl})
+        already=Product.objects.filter(name=new_name).count()
+        if(already!=0):
+            if(new_name!=product.name):
+                notAlreadyExists=False
+        if(isName==True and isUrl==True and notAlreadyExists==True):
+            product.name=new_name
+            product.url=new_url
+            product.save()
+            return render(request, 'products/productslist.html', {'products':products,'user':request.user, 'sellpairs':sellpairs_list})
+        else:
+            return render(request, 'products/product_details.html', {'product':product,'user':request.user,'isName': isName, 'isUrl':isUrl,'notAlreadyExists':notAlreadyExists})
     product= Product.objects.get(name=slug)
-    return render (request, 'products/product_details.html', {'product':product, 'user':user,'isName': isName, 'isUrl':isUrl})
+    return render (request, 'products/product_details.html', {'product':product, 'user':user,'isName': isName, 'isUrl':isUrl, 'notAlreadyExists':notAlreadyExists})
 
 def product_delete(request,slug):
     product= Product.objects.get(name=slug)
