1d64050631d2a4e7cf3b9de8ff01e74910c6abc7
adelebrgx <adele.bourgeix@gmail.com>
2020-06-08 16:40:37 +0200

    markers for shop locations on the map

--- abastos/abastos/views.py
+++ abastos/abastos/views.py
@@ -1,6 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from locations.models import Location
 
 def homepage(request):
     print(request.user)
-    return render(request,'homepage.html',{'user':request.user})
+    mapbox_accessToken = 'pk.eyJ1IjoiYWRlbGVib3VyZ2VpeCIsImEiOiJja2I2aXRiMmUwNnliMnNtbjFvMGVudGprIn0.aNQWYIpxou51xMciFZWVAQ'
+    locations=Location.objects.all()
+
+    return render(request,'homepage.html',{'user':request.user, 'token':mapbox_accessToken, 'locations':locations})
