10e2c901aedfa7c7818e36750c0b8e7ea7126a62
adelebrgx <adele.bourgeix@gmail.com>
2020-06-09 21:16:34 +0200

    marker content

--- abastos/abastos/views.py
+++ abastos/abastos/views.py
@@ -1,10 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from locations.models import Location
+from sell.models import SellPair
 
 def homepage(request):
     print(request.user)
     mapbox_accessToken = 'pk.eyJ1IjoiYWRlbGVib3VyZ2VpeCIsImEiOiJja2I2aXRiMmUwNnliMnNtbjFvMGVudGprIn0.aNQWYIpxou51xMciFZWVAQ'
     locations=Location.objects.all()
+    sellpairs=SellPair.objects.all()
 
-    return render(request,'homepage.html',{'user':request.user, 'token':mapbox_accessToken, 'locations':locations})
+    return render(request,'homepage.html',{'user':request.user, 'token':mapbox_accessToken, 'locations':locations,'sellpairs':sellpairs})
